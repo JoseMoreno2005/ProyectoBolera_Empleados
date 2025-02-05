a7ce4df70d38a8a236ccb7492f69d1912eb242b9
Jose <morenoortizjose2005@gmail.com>
2025-02-05 15:02:45 +0100

    Principal avanzado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { HorarioComponent } from './horario/horario.component';
 import { PeticionesComponent } from './componentes/peticiones/peticiones.component';
 import { FichajesComponent } from './componentes/fichajes/fichajes.component';
 import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { PrincipalComponent } from './componentes/principal/principal.component';
 
 export const routes: Routes = [
     {path:'horario',component:HorarioComponent},
     {path:'peticiones',component:PeticionesComponent},
     {path:'fichajes',component:FichajesComponent},
     {path:'perfil',component:PerfilComponent},
-    
+    {path:'',component:PrincipalComponent}
 ];
